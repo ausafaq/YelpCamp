7df3c00c9e30d849f5b7cfe36fc58cceb1972c4a
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-04-07 14:39:54 -0700

    Refactor SHOW

--- v1/app.js
+++ v1/app.js
@@ -37,7 +37,8 @@ app.get("/campgrounds", (req, res) => {
 app.post("/campgrounds", (req, res) => {
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
     Campground.create(newCampground, (err, newlyCreated) => {
         if(err) {
             console.log(err);
@@ -55,7 +56,13 @@ app.get("/campgrounds/new", (req, res) => {
 
 // SHOW - shows more info about one campground
 app.get("/campgrounds/:id", (req, res) => {
-    res.render("show");
+    Campground.findById(req.params.id, (err, foundCampground) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("show", {campground: foundCampground});
+        }
+    })
 });
 
 app.listen(3000, () => console.log(`Listening on port 3000`));
