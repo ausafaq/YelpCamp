47567ed10aeb27409b9a75213cfc108fa84dab71
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-04-10 13:03:30 -0700

    Seeding: Comments

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app        = express();
 const bodyParser = require("body-parser");
 const mongoose   = require("mongoose");
 const Campground = require("./models/campground");
-const seedDB     = require("./seeds"); 
+const seedDB     = require("./seeds");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp",  {useNewUrlParser: true });
+seedDB();
+mongoose.connect("mongodb://localhost:27017/yelp_camp_v4",  {useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
