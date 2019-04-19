05db4768bd6d57594e0f94f3091ae84020d22e7d
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-04-18 23:47:16 -0700

    Changed port

--- app.js
+++ app.js
@@ -92,5 +92,4 @@ app.post("/campgrounds/:id/comments", (req, res) => {
     })
 });
 
-
-app.listen(3000, () => console.log(`Listening on port 3000`));
+app.listen(3000, () => console.log(`Listening on port 3001`));
