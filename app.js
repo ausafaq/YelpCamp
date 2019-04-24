414e1c679212bf65dc77c6360fa65fa39a7be824
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-04-23 19:37:14 -0700

    Auth: login and logout handling

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/", (req, res) => {
     res.render("landing");
@@ -79,7 +83,7 @@ app.get("/campgrounds/:id", (req, res) => {
 // COMMENT ROUTES
 // =================
 
-app.get("/campgrounds/:id/comments/new", (req, res) => {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, (req, res) => {
     Campground.findById(req.params.id, (err, campground) => {
         if(err) {
             console.log(err);
@@ -89,7 +93,7 @@ app.get("/campgrounds/:id/comments/new", (req, res) => {
     })
 });
 
-app.post("/campgrounds/:id/comments", (req, res) => {
+app.post("/campgrounds/:id/comments", isLoggedIn, (req, res) => {
     Campground.findById(req.params.id, (err, campground) => {
         if(err) {
             console.log(err);
@@ -150,5 +154,12 @@ app.get("/logout", (req, res) => {
     req.logout();
     res.redirect("/campgrounds");
 });
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(3000, () => console.log(`Listening on port 3000`));
