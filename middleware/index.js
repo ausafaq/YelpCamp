03c0becfb850be6673eff265e777b5777e87c1b9
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-05-01 12:56:29 -0700

    Flash messages

--- middleware/index.js
+++ middleware/index.js
@@ -44,6 +44,7 @@ middlewareObj.isLoggedIn = (req, res, next) => {
     if(req.isAuthenticated()) {
         return next();
     }
+    req.flash("error", "Please login first!");
     res.redirect("/login");
 }
 
