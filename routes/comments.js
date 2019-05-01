c4d421834b6bebf75a8f53653762096b405823d7
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-04-30 18:15:35 -0700

    Destroy comment 2

--- routes/comments.js
+++ routes/comments.js
@@ -61,7 +61,13 @@ router.put("/:comment_id", (req, res) => {
 
 // Comments destroy route
 router.delete("/:comment_id", (req, res) => {
-    res.send("this is the destroybjdbfjsdbgskdjbgk");
+    Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+        if(err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
 });
 
 // middleware
